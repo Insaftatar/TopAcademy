76ceae93dc73459a802a452567847b9d713d80c8
Insaftatar <122696616+Insaftatar@users.noreply.github.com>
2023-01-16 15:48:56 +0000

    add insaf

--- test_main.cpp
+++ test_main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
+void insaf(int a,int b){
+    std::cout<<a+b;
+}
+
 int main()
 {
     std::cout<<"Hello World"<<"\n";
-    std::cout<<"Hello Insaf";
-
+    std::cout<<"Hello Insaf"<<"\n";
+    insaf(1,2);
     return 0;
 }
